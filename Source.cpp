0c287609f58b28ca1d777ab57ef3e8869c7281ca
Joey Chen <37776102+JekkuChen@users.noreply.github.com>
2020-11-14 12:11:18 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
 
 int** createBoard(int dimx, int dimy);
 void FileRead(int& max_c, int& max_f, int& dimx, int& dimy, int**& board);
 int main();
+
+
+void FileWrite(int botnum, vector<int> botposx,vector<int> botposy) { //Keep in mind, coordinate system in code is (N+1)X(M+1). JSON output needs to be in 0,0;
+	string name[12] = {"peter","alex","jekku","jessie","rob","bob","billy","cleany","chad", "ian", "ryan", "stephen"}; //Add more if you want
+	ofstream solu_file("sol.txt");
+	solu_file << "{\n\t\"robots\": [\n";
+	for (int i = 0; i < botnum-1 ; ++i) {
+		solu_file << "\t\t[\"" << name[i] << "\", [" << botposx.back() << ", " << botposy.back() << "]]," << endl;
+		botposx.pop_back();
+		botposy.pop_back();
+	}
+
+	solu_file << "\t\t[\"" << name[botnum-1] << "\", [" << botposx.back() << ", " << botposy.back() << "]]" << endl << "]," << endl;
+
+	return;
+}
 
 
 int** createBoard(int dimx, int dimy) { //Board has dim(rows + 1 by col + 1)
@@ -19,7 +37,8 @@ int** createBoard(int dimx, int dimy) { //Board has dim(rows + 1 by col + 1)
 
 
 void FileRead(int& max_c, int& max_f, int& dimx, int& dimy, int**& board) {
-	ifstream prob_file;;
+
+	ifstream prob_file;
 	prob_file.open("case2.txt");
 	if (prob_file.is_open()) {           
 		prob_file >> max_c >> max_f >> dimx >> dimy;
@@ -36,7 +55,14 @@ void FileRead(int& max_c, int& max_f, int& dimx, int& dimy, int**& board) {
 }
 
 int main() {
-	int max_c{}, max_f{}, dimx{}, dimy{},** board{};
+	int max_c{}, max_f{}, dimx{}, dimy{}, ** board{}, botnum{};
+	vector<int>startx, starty;
+	startx.push_back(1);
+	starty.push_back(1);
+	startx.push_back(1);
+	starty.push_back(2);
+	botnum = 2;
+
 	FileRead(max_c, max_f, dimx, dimy, board); //Read File and Input Variables
 
 
@@ -45,12 +71,33 @@ int main() {
 
 
 
-	for (int i = 0; i < dimx + 2; ++i) { 
+
+
+
+
+
+
+
+
+
+
+	FileWrite(botnum, startx, starty);
+
+
+
+
+
+
+
+
+
+
+	/*for (int i = 0; i < dimx + 2; ++i) { 
 		for (int j = 0; j < dimy + 2; ++j) {
 			cout << board[i][j] << " ";
 		}
 		cout << endl;
 	}
-
+	*/
 	return 0;
 }
