111e0a9728533140aad169e9e582be12e8729f53
Joey Chen <37776102+JekkuChen@users.noreply.github.com>
2020-11-14 14:15:36 -0500

    Update Source.cpp
    Finish V 1.0 Beta

--- Source.cpp
+++ Source.cpp
@@ -9,11 +9,12 @@ using namespace std;
 int** createBoard(int dimx, int dimy);
 void FileRead(int& max_c, int& max_f, int& dimx, int& dimy, int**& board);
 int main();
+void FileWriteR(int botnum, vector<int> botposx, vector<int> botposy);
+ofstream solu_file("sol.txt");
+string name[12] = { "peter","alex","jekku","jessie","rob","bob","billy","cleany","chad", "ian", "ryan", "stephen" }; //Add more if you want
 
-
-void FileWrite(int botnum, vector<int> botposx,vector<int> botposy) { //Keep in mind, coordinate system in code is (N+1)X(M+1). JSON output needs to be in 0,0;
-	string name[12] = {"peter","alex","jekku","jessie","rob","bob","billy","cleany","chad", "ian", "ryan", "stephen"}; //Add more if you want
-	ofstream solu_file("sol.txt");
+void FileWriteR(int botnum, vector<int> botposx,vector<int> botposy) { //Keep in mind, coordinate system in code is (N+1)X(M+1). JSON output needs to be in 0,0;
+	
 	solu_file << "{\n\t\"robots\": [\n";
 	for (int i = 0; i < botnum-1 ; ++i) {
 		solu_file << "\t\t[\"" << name[i] << "\", [" << botposx.back() << ", " << botposy.back() << "]]," << endl;
@@ -21,16 +22,16 @@ void FileWrite(int botnum, vector<int> botposx,vector<int> botposy) { //Keep in 
 		botposy.pop_back();
 	}
 
-	solu_file << "\t\t[\"" << name[botnum-1] << "\", [" << botposx.back() << ", " << botposy.back() << "]]" << endl << "]," << endl;
-
+	solu_file << "\t\t[\"" << name[botnum-1] << "\", [" << botposx.back() << ", " << botposy.back() << "]]" << endl << "\t]," << endl;
+	solu_file << "\t\"actions\": [" << endl;
 	return;
 }
 
 
-int** createBoard(int dimx, int dimy) { //Board has dim(rows + 1 by col + 1)
-	int** board = new int* [dimx + 2]{};
-	for (int i = 0; i < dimx + 2; ++i) { //Outside Border is only for base stations
-		board[i] = new int[dimy + 2]{};
+int** createBoard(int dimx, int dimy) { 
+	int** board = new int* [dimx]{};
+	for (int i = 0; i < dimx; ++i) { 
+		board[i] = new int[dimy]{};
 	}
 	return board;
 }
@@ -43,8 +44,8 @@ void FileRead(int& max_c, int& max_f, int& dimx, int& dimy, int**& board) {
 	if (prob_file.is_open()) {           
 		prob_file >> max_c >> max_f >> dimx >> dimy;
 		board = createBoard(dimx, dimy); //Create Board
-		for (int i = 1; i < dimx + 1; ++i) {
-			for (int j = 1; j < dimy + 1; ++j) {
+		for (int i = 0; i < dimx ; ++i) {
+			for (int j = 0; j < dimy; ++j) {
 				prob_file >> board[i][j];
 			}
 		}
@@ -54,50 +55,43 @@ void FileRead(int& max_c, int& max_f, int& dimx, int& dimy, int**& board) {
 
 }
 
+void ActionR(int max_c, int dimx, int dimy, int x, int y, int botnum, int**board) { // 1 is Move, 2 is Clean; 
+	for (int i = dimx-1; i >= 0; --i) {
+		solu_file << "\t\t[\"" << name[botnum] << "\", \"move\", [" << i << ", " << y << "]]," <<endl;
+		solu_file << "\t\t[\"" << name[botnum] << "\", \"clean\"," << board[i][y] <<"]," << endl;
+	}
+	for (int i = 1; i <= dimx; ++i) {
+		solu_file << "\t\t[\"" << name[botnum] << "\", \"move\", [" << i << ", " << y << "]]," << endl;
+	}
+	
+}
+
+
 int main() {
 	int max_c{}, max_f{}, dimx{}, dimy{}, ** board{}, botnum{};
 	vector<int>startx, starty;
-	startx.push_back(1);
-	starty.push_back(1);
-	startx.push_back(1);
-	starty.push_back(2);
-	botnum = 2;
 
 	FileRead(max_c, max_f, dimx, dimy, board); //Read File and Input Variables
+	int* locx = new int[dimy] {};
+	int* locy = new int[dimy] {};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	FileWrite(botnum, startx, starty);
-
-
-
-
-
-
-
-
-
-
-	/*for (int i = 0; i < dimx + 2; ++i) { 
-		for (int j = 0; j < dimy + 2; ++j) {
-			cout << board[i][j] << " ";
-		}
-		cout << endl;
+	for (int i = 0; i < dimy; i++) {
+		startx.push_back(dimx);
+		starty.push_back(i);
+		locx[i] = dimx;
+		locy[i] = i;
+		botnum++;
 	}
-	*/
+
+	FileWriteR(botnum, startx, starty);
+
+	for (int i = 0; i < dimy; i++) {
+		ActionR(max_c, dimx, dimy, locx[i], locy[i], dimy-i-1,board);
+	}
+	solu_file << "\t\t[\"" << name[0] << "\", \"clean\"," << 0 << "]" << endl << "\t]" << endl << "}";
+
+
+		
 	return 0;
 }
